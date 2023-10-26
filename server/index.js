a435583ad90fc8027dddd25042087e63a02ced57
KingKennyboy <95037426+KingKennyboy@users.noreply.github.com>
2023-10-26 15:25:04 -0500

    added databse connection

--- server/index.js
+++ server/index.js
@@ -3,6 +3,13 @@ import mysql from "mysql";
 
 const app = express()
 
+const db = mysql.createConnection({
+    host:"medicalclinic16.mysql.database.azure.com",
+    user:"medclinic_admin",
+    password:"UmaTeam16",
+    database:"hospital"
+})
+
 app.listen(8800,()=>{
     console.log("Connected to server!")
 })
