1461fd23bf13bbaf1e726ac1a902b2e937d1a405
KingKennyboy <95037426+KingKennyboy@users.noreply.github.com>
2023-10-31 13:08:32 -0500

    pull from database

--- server/index.js
+++ server/index.js
@@ -13,6 +13,16 @@ const db = mysql.createConnection({
 app.get("/", (req,res)=>{
     res.json("this is the backend")
 })
+app.get("/doctor", (req,res)=>{
+    const q = "SELECT * FROM doctor"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+//if theres an auth problem
+//ALTER USER 'medclinic_admin'@'medicalclinic16.mysql.database.azure.com' IDENTIFIED WITH mysql_native_password BY 'UmaTeam16'
 
 app.listen(8800,()=>{
     console.log("Connected to server!")
